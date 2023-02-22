9fb79373291e17b8b1f92f4cbfd04d0507e3f6fb
AndresHadechine <andreshadechine@gmail.com>
2023-02-22 09:31:39 -0500

    Se implementa Mockito

--- src/test/java/org/example/AritmeticaTest.java
+++ src/test/java/org/example/AritmeticaTest.java
@@ -1,15 +1,26 @@
 package org.example;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class AritmeticaTest {
 
-    static Aritmetica aritmetica;
+    @InjectMocks
+    public static Aritmetica aritmetica;
+
+    @Mock
+    private CalculadoraAppService service;
 
     @BeforeAll
     public static void initClass(){
         aritmetica = new Aritmetica();
     }
+
     @AfterAll
     public static void finishClass(){
         aritmetica = null;
@@ -61,6 +72,41 @@ class AritmeticaTest {
              aritmetica.division(4, 0);
          });
         Assertions.assertEquals("Divisor no puede ser cero", exception.getMessage());
+    }
+
+    @Test
+    public void testPotencia() throws Exception {
+        //Arrange
+        String usuario = "andres";
+        String contrasena = "hadechine";
+        int base = 2;
+        int exponente = 2;
+        Mockito.when(service.login(usuario,contrasena)).thenReturn(true);
+        Mockito.when(service.potencia(base, exponente)).thenReturn(4);
+
+        //Act
+        int resultado = aritmetica.potencia(usuario, contrasena, base, exponente);
+
+        //Assert
+        Assertions.assertEquals(4, resultado);
+    }
+
+    @Test
+    public void testPotenciaLoginInvalido() throws Exception {
+        //Arrange
+        String usuario = "usuario";
+        String contrasena = "invalido";
+        int base = 2;
+        int exponente = 2;
+        Mockito.when(service.login(usuario,contrasena)).thenReturn(false);
+
+        //Act
+        Exception exception = Assertions.assertThrows(Exception.class, () -> {
+            aritmetica.potencia(usuario, contrasena, base, exponente);
+        });
+
+        //Assert
+        Assertions.assertEquals("Usuario o la contraseña son inválidos", exception.getMessage());
     }
 
 }
